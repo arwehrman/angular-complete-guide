106dd446d2d58dde166c5ecc9ba022a89a745a8b
amanda_wehrman <amanda.wehrman@thinkbrownstone.com>
2020-01-28 10:22:10 -0500

    finished Assignment 3

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  displayDetails = false;
+  log = [];
+
+  onDisplayDetails() {
+    this.displayDetails = !this.displayDetails;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
 }
